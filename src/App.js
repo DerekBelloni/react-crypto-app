60bcb5ea692520c299975b8dc92c2a6a0f9330cf
Derek <derekbelloni10@gmail.com>
2022-05-05 18:35:06 -0600

    main components created, routes established

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
+import './App.css';
 
 
 const App = () => {
@@ -9,12 +15,38 @@ const App = () => {
     <div className="app">
       <div className="navbar">
         <Navbar />
-        <div className="main">
-          <div className="footer"></div>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+
+
+
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer"  >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
         </div>
       </div>
     </div>
   )
 }
+
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <>
+    <Router>
+      <App />
+    </Router>
+  </>
 );
